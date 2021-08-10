0019614e33c40a4d7d8cf664fa2f7f79eb3d8828
Emiliano <emilianop96@hotmail.com>
2021-08-10 20:15:46 -0300

    create custom exception filter

---
+++ src/jobs/filters/http-exception.filter.ts
@@ -0,0 +1,22 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(ex: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const req = ctx.getRequest<Request>();
+    const res = ctx.getResponse<Response>();
+    const status = ex.getStatus();
+    res.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: req.url,
+    });
+  }
+}
