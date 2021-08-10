c31e443c854dacbd9db402951b28b67e6e3a5365
Emiliano <emilianop96@hotmail.com>
2021-08-10 13:15:12 -0300

    create services. use services in controller

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,32 +1,43 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { JobsService } from './jobs.service';
+import { JobDto } from './dtos/job.dto';
+import { IJob } from './interfaces/job.interface';
 
 @Controller('jobs')
 export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
   @Get()
-  findAll(): string {
-    return 'findAll';
+  findAll(): Promise<IJob[]> {
+    return this.jobsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() params): string {
-    console.log(params.id);
-    return 'findOne';
+  findOne(@Param('id') id: string): Promise<IJob> {
+    return this.jobsService.findOne(id);
   }
 
   @Post()
-  create(): string {
-    return 'create';
+  create(@Body() job: JobDto): Promise<IJob> {
+    return this.jobsService.create(job);
   }
 
   @Put(':id')
-  update(@Param() params): string {
-    console.log(params.id);
-    return 'update';
+  update(@Param('id') id: string, @Body() job: JobDto): Promise<IJob> {
+    return this.jobsService.update(id, job);
   }
 
   @Delete(':id')
-  delete(@Param() params): string {
-    console.log(params.id);
-    return 'delete';
+  delete(@Param('id') id: string): Promise<IJob> {
+    return this.jobsService.delete(id);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Job } from './schemas/job.schema';
+import { IJob } from './interfaces/job.interface';
 
 @Injectable()
-export class JobsService {}
+export class JobsService {
+  constructor(@InjectModel(Job.name) private jobModel: Model<IJob>) {}
+
+  async findAll(): Promise<IJob[]> {
+    return await this.jobModel.find();
+  }
+
+  async findOne(id: string): Promise<IJob> {
+    return await this.jobModel.findById(id);
+  }
+
+  async create(job: IJob): Promise<IJob> {
+    const newJob = new this.jobModel(job);
+    return await newJob.save();
+  }
+
+  async update(id: string, job: IJob): Promise<IJob> {
+    return await this.jobModel.findByIdAndUpdate(id, job, { new: true });
+  }
+
+  async delete(id: string): Promise<IJob> {
+    return await this.jobModel.findByIdAndDelete(id);
+  }
+}
