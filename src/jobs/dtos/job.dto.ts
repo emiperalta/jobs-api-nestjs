7944c7882b4bb9cc6a0142b79c57b2a590cf976f
Emiliano <emilianop96@hotmail.com>
2021-08-11 02:07:24 -0300

    add documentation

--- src/jobs/dtos/job.dto.ts
+++ src/jobs/dtos/job.dto.ts
@@ -1,9 +1,17 @@
 import { IsString, IsInt } from 'class-validator';
 
 export class JobDto {
+  /**
+   * The job title
+   * @example "C++ Developer"
+   */
   @IsString()
   readonly title: string;
 
+  /**
+   * The job salary
+   * @example 5000
+   */
   @IsInt()
   readonly salary: number;
 }
--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,14 +1,24 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
+  HttpCode,
   NotFoundException,
   Param,
   Post,
   Put,
   UseFilters,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { JobsService } from './jobs.service';
 import { JobDto } from './dtos/job.dto';
@@ -16,17 +26,21 @@ import { IJob } from './interfaces/job.interface';
 import { HttpExceptionFilter } from '../filters/http-exception.filter';
 import { ValidationPipe } from '../pipes/validation.pipe';
 
+@ApiTags('jobs')
 @Controller('jobs')
 @UseFilters(HttpExceptionFilter)
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Get()
+  @ApiOkResponse({ description: 'A list of all jobs' })
   async findAll(): Promise<IJob[]> {
     return await this.jobsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'A job with specific ID' })
+  @ApiNotFoundResponse({ description: 'Job not found' })
   async findOne(@Param('id') id: string): Promise<IJob> {
     try {
       const job = await this.jobsService.findOne(id);
@@ -38,20 +52,36 @@ export class JobsController {
   }
 
   @Post()
+  @ApiCreatedResponse({ description: 'Job successfully created' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   create(@Body(new ValidationPipe()) job: JobDto): Promise<IJob> {
     return this.jobsService.create(job);
   }
 
   @Put(':id')
-  update(
+  @HttpCode(204)
+  @ApiNoContentResponse({ description: 'Job updated successfuly' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  async update(
     @Param('id') id: string,
     @Body(new ValidationPipe()) job: JobDto,
-  ): Promise<IJob> {
-    return this.jobsService.update(id, job);
+  ): Promise<void> {
+    try {
+      return await this.jobsService.update(id, job);
+    } catch (err) {
+      throw new BadRequestException();
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): Promise<IJob> {
-    return this.jobsService.delete(id);
+  @HttpCode(204)
+  @ApiNoContentResponse({ description: 'Job deleted successfuly' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  async delete(@Param('id') id: string): Promise<void> {
+    try {
+      return await this.jobsService.delete(id);
+    } catch (err) {
+      throw new BadRequestException();
+    }
   }
 }
